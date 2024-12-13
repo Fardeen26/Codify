8e0c727616f7c7ca6633f39f88928433cca084ff
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-12-13 22:36:46 +0530

    fix: ui changes

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { toPng } from "html-to-image";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "./ui/textarea";
 import { themes } from "@/lib/theme";
-import { Label } from "./ui/label";
 
 
 export default function CodeEditor() {
@@ -17,7 +16,7 @@ export default function CodeEditor() {
     const [theme, setTheme] = useState<typeof vscDarkPlus>(vscDarkPlus);
     const [language, setLanguage] = useState<string>("javascript");
     const [fontSize, setFontSize] = useState<number>(16);
-    const [backgroundColor, setBackgroundColor] = useState<string>("#575F68");
+    const [backgroundColor, setBackgroundColor] = useState<string>("#EAE0A4");
     const [isBackgroundHidden, setIsBackgroundHidden] = useState(false)
 
     const exportAsImage = () => {
@@ -46,7 +45,7 @@ export default function CodeEditor() {
         <div className="flex flex-col gap-6 min-h-[90vh] items-center pb-4 justify-center dark:text-white max-sm:px-2">
             <div className="w-full max-w-4xl space-y-4 max-sm:space-y-4">
                 <Textarea
-                    className="w-full h-40 p-4 rounded-md border dark:text-white border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none text-gray-800"
+                    className="w-full h-40 p-4 rounded-md border dark:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none text-gray-800 border-black dark:border-white"
                     value={code}
                     onChange={(e) =>
                         setCode(e.target.value)
@@ -55,15 +54,14 @@ export default function CodeEditor() {
 
                 />
 
-                <div className="flex flex-wrap gap-4 max-sm:gap-2 items-center justify-between">
+                <div className="flex flex-wrap gap-12 max-sm:gap-2 items-center justify-between max-sm:justify-center">
                     <div className="flex items-center gap-2">
-                        <Label className="text-[13px]">Theme:</Label>
                         <Select
                             onValueChange={(value: string) => {
                                 setTheme(themes[value]);
                             }}
                         >
-                            <SelectTrigger className="w-35 h-8 text-xs">
+                            <SelectTrigger className="w-35 h-8 text-xs border-black dark:border-white">
                                 <SelectValue placeholder="Select Theme" />
                             </SelectTrigger>
                             <SelectContent>
@@ -77,13 +75,12 @@ export default function CodeEditor() {
                     </div>
 
                     <div className="flex items-center gap-2">
-                        <Label className="text-[13px]">Language:</Label>
                         <Select
                             onValueChange={(value: string) => {
                                 setLanguage(value);
                             }}
                         >
-                            <SelectTrigger className="w-35 h-8 text-xs">
+                            <SelectTrigger className="w-35 h-8 text-xs border-black dark:border-white">
                                 <SelectValue placeholder="Select Language" />
                             </SelectTrigger>
                             <SelectContent>
@@ -100,52 +97,51 @@ export default function CodeEditor() {
                     </div>
 
                     <div className="flex items-center gap-2">
-                        <Label className="text-[13px]">Background:</Label>
                         <Input
                             type="color"
                             value={backgroundColor}
                             onChange={(e) =>
                                 setBackgroundColor(e.target.value)
                             }
-                            className="w-16 h-8"
+                            className="w-16 h-8 border-black dark:border-white"
                         />
                     </div>
 
                     <div className="flex items-center gap-2">
-                        <Label className="text-[13px]">Font Size:</Label>
                         <Input
                             type="number"
                             value={fontSize}
                             onChange={(e) =>
                                 setFontSize(parseInt(e.target.value, 10) || 16)
                             }
-                            className="w-16 h-8 font-xs"
+                            className="w-16 h-8 font-xs border-black dark:border-white"
                         />
                     </div>
-                </div>
-                <div className="flex justify-between">
-                    <Button onClick={handleBackgroundHidden} className="text-xs h-8 dark:bg-white dark:text-black dark:hover:bg-gray-100">
-                        {
-                            isBackgroundHidden ? 'Add Background' : 'Hide Background'
-                        }
-                    </Button>
+                    <div className="flex justify-between gap-12 max-sm:gap-2">
+                        <Button onClick={handleBackgroundHidden} className="text-xs h-8 bg-transparent border border-black dark:border-white text-black hover:bg-gray-200 dark:hover:bg-gray-900 dark:text-white">
+                            {
+                                isBackgroundHidden ? 'Add Background' : 'Hide Background'
+                            }
+                        </Button>
 
-                    <Button onClick={exportAsImage} className="ml-auto text-xs h-8 dark:bg-white dark:text-black dark:hover:bg-gray-100">
-                        Export as Image
-                    </Button>
+                        <Button onClick={exportAsImage} className="ml-auto text-xs h-8 bg-transparent border border-black dark:border-white text-black hover:bg-gray-200 dark:hover:bg-gray-900 dark:text-white">
+                            Export as Image
+                        </Button>
+                    </div>
                 </div>
+
             </div>
 
             <div
                 id="code-preview"
-                className={`w-full max-w-4xl p-4 rounded-md border border-gray-200 dark:border-none shadow-lg relative ${isBackgroundHidden ? '!bg-transparent shadow-none border-none' : ''
+                className={`w-full mt-5 max-w-4xl p-10 max-sm:p-3 rounded-md border border-gray-200 dark:border-none bg-yellow-100 shadow-lg relative ${isBackgroundHidden ? '!bg-transparent shadow-none border-none' : ''
                     }`}
                 style={{ backgroundColor }}
             >
-                <div className="flex items-center space-x-2 mt-1 absolute top-1 left-5">
-                    <span className="w-[10px] h-[10px] rounded-full bg-red-500"></span>
-                    <span className="w-[10px] h-[10px] rounded-full bg-yellow-500"></span>
-                    <span className="w-[10px] h-[10px] rounded-full bg-green-500"></span>
+                <div className="flex items-center space-x-2 mt-1 absolute top-6 max-sm:top-0 left-25 max-sm:left-4">
+                    <span className="w-[9px] h-[9px] max-sm:w-2 max-sm:h-2 rounded-full bg-red-500"></span>
+                    <span className="w-[9px] h-[9px] max-sm:w-2 max-sm:h-2 rounded-full bg-yellow-500"></span>
+                    <span className="w-[9px] h-[9px] max-sm:w-2 max-sm:h-2 rounded-full bg-green-500"></span>
                 </div>
 
                 <SyntaxHighlighter
@@ -154,12 +150,12 @@ export default function CodeEditor() {
                     customStyle={{
                         fontSize: `${fontSize}px`,
                         borderRadius: "8px",
-                        whiteSpace: "pre-wrap",
-                        wordBreak: "break-word",
-                        overflow: "hidden",
-                        display: "block",
-                        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4)'
+                        padding: '18px 0 18px 15px',
+                        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4)',
+                        overflow: 'hidden'
                     }}
+                    wrapLongLines
+                    showLineNumbers
                 >
                     {code || "// Designed and Developed by Fardeen..."}
                 </SyntaxHighlighter>
